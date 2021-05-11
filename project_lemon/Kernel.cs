947ba4801aabfcc7282b94b15101250b33b4924e
bad-codr <76945439+bad-codr@users.noreply.github.com>
2021-05-11 02:21:39 -0700

    Add files via upload

---
+++ project_lemon/Kernel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace project_lemon
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Loaded project lemon. developer test 1");
+        }
+
+        protected override void Run()
+        {
+            Console.Write("setup: ");
+            var input = Console.Read();
+            {
+
+            }
+        }
+    }
+}
