9a2a71d39b8ae4215f2060408fd75747bf828fd1
bad-codr <76945439+bad-codr@users.noreply.github.com>
2021-05-19 00:37:16 -0700

    Create PGUI.cs

---
+++ PrismProject/PGUI.cs
@@ -0,0 +1,29 @@
+using Cosmos.System.Graphics;
+
+namespace PrismProject
+{
+    class PGUI
+    {
+        public static Canvas canvas;
+        public static int screenX = canvas.Mode.Rows;
+        public static int screenY = canvas.Mode.Columns;
+
+        public static void boot_screen()
+        {
+            canvas = FullScreenCanvas.GetFullScreenCanvas();
+            canvas.Display();
+            Utils.Playjingle();
+            canvas.Clear(System.Drawing.Color.White);
+            canvas.DrawFilledRectangle(new Pen(System.Drawing.Color.DarkGray), 0, 0, screenX, screenY / 8);
+            while (true)
+            {
+                int a = 1;
+            }
+        }
+
+        public static void message()
+        {
+            //coming soon.
+        }
+    }
+}
