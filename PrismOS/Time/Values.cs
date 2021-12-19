6af7074eef025caa03bc4a5ebc974f485addccab
Evan Lawrence <mailing.terminal.cs@gmail.com>
2021-12-19 09:35:12 -0800

    revert unix time stamp values

--- PrismOS/Time/Values.cs
+++ PrismOS/Time/Values.cs
@@ -4,6 +4,7 @@ namespace PrismOS.Time
 {
     public static class Values
     {
-        public static int UnixTimestampMils { get => int.Parse(DateTime.UtcNow.ToString("fffffff")); }
+        public static int UnixTimestamp { get => (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds; }
+        public static int UnixTimestampMils { get => (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalMilliseconds; }
     }
 }
