7b70ed3bce6491a11b67ac8cb740549705f6b217
Evan Lawrence <mailing.terminal.cs@gmail.com>
2021-12-22 19:33:37 -0800

    We do a little refactoring

--- PrismOS/Kernel.cs
+++ PrismOS/Kernel.cs
@@ -1,34 +1,20 @@
-using Pen = Cosmos.System.Graphics.Pen;
-using Font = Cosmos.System.Graphics.Fonts.PCScreenFont;
 using static PrismOS.Storage.Storage;
 using static PrismOS.Network.Network;
-using System;
+using System.Collections.Generic;
 
 namespace PrismOS
 {
     public class Kernel : Cosmos.System.Kernel
     {
+        public static List<UI.SaltUI.Window> Windows = new();
+
         protected override void Run()
         {
-            try
-            {
-                InitVFS();
-                InitNet();
+            InitVFS();
+            InitNet();
 
-                Network.NWeb.NWebClient x = new(new Cosmos.System.Network.IPv4.Address(0, 0, 0, 0), "ID1");
-                x.Post("Test.txt", System.Text.Encoding.UTF8.GetBytes("Hello, this is a test using the NWeb client and server!"));
-                Console.WriteLine(x.Get("Test.txt"));
-            }
-            catch(Exception EX)
-            {
-                UI.Forms.Canvas.Clear();
-                UI.Forms.Canvas.DrawString(
-                    "Error! " + EX.Message,
-                    Font.Default,
-                    new Pen(System.Drawing.Color.Red),
-                    (UI.Forms.Canvas.Mode.Columns / 2) - (Font.Default.Width * (15 + EX.Message.Length) / 2),
-                    (UI.Forms.Canvas.Mode.Rows / 2) - (Font.Default.Height * 2 / 2));
-            }
+            UI.SaltUI.Window xDisplay = new(50, 50, 100, 50);
+            xDisplay.Draw();
         }
     }
 }
---
+++ PrismOS/UI/SaltUI.cs
@@ -0,0 +1,61 @@
+﻿using Cosmos.System.Graphics;
+
+namespace PrismOS.UI
+{
+    public static class SaltUI
+    {
+        public static Canvas Canvas { get; set; } = FullScreenCanvas.GetFullScreenCanvas();
+        internal delegate void Event();
+
+        public interface IForm
+        {
+            void Draw();
+            void ClickDown();
+            void ClickUp();
+            void Hover();
+            void Destroy();
+        }
+
+        public class Window : IForm
+        {
+            internal Event OnClickDown;
+            internal Event OnClickUp;
+            internal Event OnHover;
+            internal Event OnDraw;
+            internal Event OnRemove;
+            internal Event OnAdd;
+
+            public int X, Y, Width, Height;
+
+            public Window(int aX, int aY, int aWidth, int aHeight)
+            {
+                X = aX;
+                Y = aY;
+                Width = aWidth;
+                Height = aHeight;
+            }
+
+            public void Draw()
+            {
+                OnDraw();
+                Canvas.DrawFilledRectangle(new Pen(System.Drawing.Color.White), X, Y, Width, Height);
+            }
+            public void ClickDown()
+            {
+                OnClickDown();
+            }
+            public void ClickUp()
+            {
+                OnClickUp();
+            }
+            public void Hover()
+            {
+                OnHover();
+            }
+            public void Destroy()
+            {
+                OnRemove();
+            }
+        }
+    }
+}
