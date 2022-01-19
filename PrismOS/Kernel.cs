4ca070b5376154c2c50bed9133276858782a2cd0
Evan Lawrence <mailing.terminal.cs@gmail.com>
2022-01-19 13:51:00 -0800

    re-add mouse

--- PrismOS/Kernel.cs
+++ PrismOS/Kernel.cs
@@ -18,6 +18,9 @@ namespace PrismOS
             int W2 = Canvas.Width / 2;
             int H2 = Canvas.Height / 2;
 
+            Cosmos.System.MouseManager.ScreenWidth = (uint)Canvas.Width;
+            Cosmos.System.MouseManager.ScreenHeight = (uint)Canvas.Height;
+
             while (true)
             {
                 if (System.DateTime.Now.Second != LT.Second)
@@ -37,7 +40,7 @@ namespace PrismOS
 
                 Canvas.Clear(Color.Green);
                 Canvas.DrawFilledRectangle(0, 0, Canvas.Width, Default.Height + 6, Color.Gray);
-                Canvas.DrawString(Canvas.Width - (Default.Width * Time.Length) - 5, 2, Default, Time, Color.White);
+                Canvas.DrawString(Canvas.Width - (Default.Width * Time.Length) - 5, 3, Default, Time, Color.White);
                 Canvas.DrawString(0, 0, Default, Canvas.FPS + " FPS", Color.White);
                 Canvas.DrawFilledRectangle(W2 - 100, H2 - 100, 200, H2 + 100, Color.FromArgb(100, 25, 25, 25));
                 Canvas.DrawFilledRectangle(W2 - 100, H2 - 100, 200, H2 - 70, Color.FromArgb(255, 25, 25, 25));
@@ -45,6 +48,7 @@ namespace PrismOS
                 Canvas.DrawAngledLine(W2, H2, System.DateTime.Now.Hour, 20, Color.Black);
                 Canvas.DrawAngledLine(W2, H2, System.DateTime.Now.Minute, 40, Color.Black);
                 Canvas.DrawAngledLine(W2, H2, System.DateTime.Now.Second, 60, Color.Black);
+                Canvas.DrawFilledCircle((int)Cosmos.System.MouseManager.X, (int)Cosmos.System.MouseManager.Y, 3, Color.Red);
                 Canvas.Update();
             }
         }
