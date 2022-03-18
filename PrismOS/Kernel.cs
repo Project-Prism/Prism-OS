8d4366dbfc24c208d74fb7ed504c1668f1677377
Evan Lawrence <mailing.terminal.cs@gmail.com>
2022-03-18 13:50:23 -0700

    add http server (not working :sob:)

--- PrismOS/Kernel.cs
+++ PrismOS/Kernel.cs
@@ -1,6 +1,5 @@
 using static PrismOS.Libraries.Graphics.ContentPage;
 using Cosmos.System.Network.IPv4.UDP.DHCP;
-using Mouse = Cosmos.System.MouseManager;
 using Cosmos.System.Network.IPv4.TCP;
 using Cosmos.System.FileSystem.VFS;
 using PrismOS.Libraries.Graphics;
@@ -21,11 +20,12 @@ namespace PrismOS
         public static ContentPage Page1 = new(50, 50, 250, 100, 0, "Window1", null, new()
         {
             new(5, (Canvas.Font.Default.Height + 2) * 0, 0, 0, 0, "FPS: 0", null, Page1, 0x01),
-            new(5, (Canvas.Font.Default.Height + 2) * 1, 0, 0, 0, "Runtime: .Net 5.0", null, Page1, 0x01),
-            new(5, (Canvas.Font.Default.Height + 2) * 2, 100, 14, 0, "Reboot", null, Page1, 0x02, (ref Element This) => { Cosmos.System.Power.Reboot(); }),
-            new(5, (Canvas.Font.Default.Height + 2) * 3, 100, 14, 0, "Shutdown", null, Page1, 0x02, (ref Element This) => { Cosmos.System.Power.Shutdown(); }),
+            new(5, (Canvas.Font.Default.Height + 2) * 1, 0, 0, 0, "Server status: Offline", null, Page1, 0x01),
+            new(5, (Canvas.Font.Default.Height + 2) * 2, 100, 14, 0, "Start server", null, Page1, 0x02, (ref Element This) => { if (!Started) { TCP.Start(); This.Text = "Server status: Online"; Started = true; } else { TCP.Stop(); This.Text = "Server status: Offline"; Started = false; } }),
         });
         public static DateTime BootTime = DateTime.UtcNow;
+        public static TcpListener TCP = new(80);
+        public static bool Started = false;
 
         protected override void BeforeRun()
         {
@@ -43,9 +43,16 @@ namespace PrismOS
                 Canvas.Clear(Color.CoolGreen);
                 Page1.Update(Canvas);
                 Page1.Children[0].Text = "FPS: " + Canvas.FPS;
+                if (Started)
+                {
+                    TcpClient Client = TCP.AcceptTcpClient(5);
+                    if (Client != null)
+                    {
+                        Client.Send(System.Text.Encoding.ASCII.GetBytes(Libraries.Network.HTTP.GenerateHTTP("<title>Prism OS</title><button OnClick=\"alert(1)\">Click me!</button>")));
+                    }
+                }
                 Canvas.DrawFilledRectangle(0, Canvas.Height - 25, Canvas.Width, 25, 0, Color.StackOverflowBlack);
                 Canvas.DrawString(5, Canvas.Height - Canvas.Font.Default.Height, Strings_EN.OSMessage, Color.White);
-                Canvas.DrawBezierLine(50, 50, (int)Mouse.X, (int)Mouse.Y, 200, 200, Color.RubyRed);
                 Canvas.Update();
             }
             catch { }
---
+++ PrismOS/Libraries/Network/HTTP.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PrismOS.Libraries.Network
+{
+    public static class HTTP
+    {
+        public static string GenerateHTTP(string Contents)
+        {
+            return
+                "HTTP / 1.1 200 OK" +
+                $"Date: {DateTime.Now:ddd, d MMM yyyy h:mm:ss}\n" +
+                "Server: Cosmos Devkit / .Net 5(Prism OS)\n" +
+                "Content - Type: text / html\n" +
+                "Connection: Closed;\n" + Contents;
+        }
+    }
+}
