fe5d1c444c395c6b9159d51b706e263b00ec7391
Evan Lawrence <evanlawrence@cyberfear.com>
2022-05-31 23:07:19 -0700

    increase title height

--- PrismOS/Libraries/Graphics/GUI/Window.cs
+++ PrismOS/Libraries/Graphics/GUI/Window.cs
@@ -15,12 +15,12 @@ namespace PrismOS.Libraries.Graphics.GUI
         {
             if (Visible)
             {
-                Canvas.DrawFilledRectangle(X, Y - (TitleVisible ? 15 : 0), Width, Height, Radius, Theme.Background);
+                Canvas.DrawFilledRectangle(X, Y - (TitleVisible ? 20 : 0), Width, Height, Radius, Theme.Background);
                 if (TitleVisible)
                 {
-                    Canvas.DrawFilledRectangle(X, Y - 15, Width, 15, Radius, Theme.Accent);
-                    Canvas.DrawString(X + (Width / 2), Y - 15, Text, Font, Theme.Foreground, true);
-                    Canvas.DrawRectangle(X, Y - (TitleVisible ? 15 : 0), Width - 1, Height - 1, Radius, Theme.Foreground);
+                    Canvas.DrawFilledRectangle(X, Y - 20, Width, 20, Radius, Theme.Accent);
+                    Canvas.DrawString(X + (Width / 2), Y - (Height / 2), Text, Font, Theme.Foreground, true);
+                    Canvas.DrawRectangle(X, Y - 20, Width - 1, Height - 1, Radius, Theme.Foreground);
                 }
             }
         }
