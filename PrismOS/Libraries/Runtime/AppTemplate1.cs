e3f871071c529ec573c13104703d8e0f4be8ec40
Evan Lawrence <evanlawrence@cyberfear.com>
2022-07-30 15:21:15 -0700

    Minor Fixes

--- PrismOS/Libraries/Runtime/AppTemplate1.cs
+++ PrismOS/Libraries/Runtime/AppTemplate1.cs
@@ -68,7 +68,7 @@ namespace PrismOS.Libraries.Runtime
                     E.Camera.Position.Z += 5;
                     break;
                 case ConsoleKeyEx.S:
-                    E.Camera.Position.Z =- 5;
+                    E.Camera.Position.Z -= 5;
                     break;
                 case ConsoleKeyEx.A:
                     E.Camera.Position.X -= 5;
