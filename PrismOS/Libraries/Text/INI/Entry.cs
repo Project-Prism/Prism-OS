9ed953e13080f879371baab7b59be1eb6b74e681
Evan Lawrence <evanlawrence@cyberfear.com>
2022-07-31 19:26:27 -0700

    Implement INI Parser

---
+++ PrismOS/Libraries/Text/INI/Entry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PrismOS.Libraries.Text.INI
+{
+    public struct Entry
+    {
+        public Entry(Type Type, string Name, object Value)
+        {
+            this.Type = Type;
+            this.Name = Name;
+            this.Value = Value;
+        }
+
+        public Type Type { get; set; }
+        public string Name { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ PrismOS/Libraries/Text/INI/INIFile.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace PrismOS.Libraries.Text.INI
+{
+    public class INIFile : Dictionary<string, List<Entry>>
+    {
+        public INIFile(string S)
+        {
+            string Selected = "";
+            string Builder = "";
+
+            for (int I = 0; I < S.Length; I++)
+            {
+                if (S[I] == ' ' || S[I] == '\n' || S[I] == '\r')
+                {
+                    continue;
+                }
+                if (S[I] == '[')
+                {
+                    while (S[++I] != ']')
+                    {
+                        Builder += S[I];
+                    }
+                    Add(Builder, new());
+                    Selected = Builder;
+                    Builder = "";
+                    continue;
+                }
+                if (S[I] == '=')
+                {
+                    string Name = Builder;
+                    string Value = "";
+                    Builder = "";
+                    while (S[I] != '\n' && S[I] != '\r' && S[I] != '\0' && I < S.Length - 1)
+                    {
+                        Value += S[++I];
+                    }
+                    if (Selected.Length != 0)
+                    {
+                        if (int.TryParse(Value, out var N))
+                        {
+                            this[Selected].Add(new(typeof(int), Name, N));
+                        }
+                        else
+                        {
+                            this[Selected].Add(new(typeof(string), Name, Value));
+                        }
+                    }
+                    continue;
+                }
+                if (S[I] == '#')
+                {
+                    while (S[++I] != '\n')
+                    {
+                    }
+                    continue;
+                }
+
+                Builder += S[I];
+            }
+        }
+    }
+}
