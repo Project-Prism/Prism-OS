0049f8393751e1b76e739aab1d8ab9064ad360d7
Evan Lawrence <evanlawrence@cyberfear.com>
2022-08-11 20:26:25 -0700

    remove broken code, for later

---
+++ PrismOS/Libraries/Resource/Text/XML/Lexer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace PrismOS.Libraries.Resource.Text.XML
+{
+    public class Lexer<T>
+    {
+        public Lexer(string XML)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
