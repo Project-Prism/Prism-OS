8ca65bcc2de4ee68ee3c3b6fb0b82c9b6d766f70
Evan Lawrence <evanlawrence@cyberfear.com>
2022-08-13 15:51:28 -0700

    Add ImageButton

---
+++ PrismOS/Libraries/UI/ImageButton.cs
@@ -0,0 +1,24 @@
+﻿using PrismOS.Libraries.Graphics;
+
+namespace PrismOS.Libraries.UI
+{
+    public class ImageButton : Button
+    {
+        public FrameBuffer Source { get; set; }
+
+        public override void OnDraw(FrameBuffer Buffer)
+        {
+            if (Source == null)
+            {
+                if (Source.Width != Width || Source.Height != Height)
+                {
+                    Source = Source.Resize(Width, Height);
+                }
+
+                this.Buffer.DrawImage(0, 0, Source, true);
+
+                Buffer.DrawImage(X, Y, this.Buffer, true);
+            }
+        }
+    }
+}
