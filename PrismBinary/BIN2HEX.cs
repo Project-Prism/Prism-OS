fbb44573f0bde5226292b570f6e0aa324b62d98c
Evan Lawrence <evanlawrence@cyberfear.com>
2022-10-04 17:02:06 -0700

    Impliment BIN2HEX

---
+++ PrismBinary/BIN2HEX.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace PrismBinary
+{
+	public static class BIN2HEX
+	{
+		public static string Main(byte[] Buffer)
+		{
+			StringBuilder Builder = new(Buffer.Length * 2);
+			for (int I = 0; I < Buffer.Length; I++)
+			{
+				Builder.AppendFormat("{0:x2}", Buffer[I]);
+			}
+			return Builder.ToString();
+		}
+	}
+}
