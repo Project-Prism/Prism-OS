d80891fef8587f5ab5dab8896dd4ff547701e0cf
Evan Lawrence <evanlawrence@cyberfear.com>
2022-10-26 16:14:34 -0700

    a

---
+++ PrismTools/MathEx.cs
@@ -0,0 +1,50 @@
+﻿namespace PrismTools
+{
+	public static class MathEx
+	{
+		public static string Eval(string Input)
+		{
+			string Temporary = "";
+			string Result = "0";
+
+			for (int I = 0; I < Input.Length; I++)
+			{
+				if (Input[I] == ' ' || Input[I] == '\n' || Input[I] == '\0')
+				{
+					continue;
+				}
+				if (ulong.TryParse(Input[I].ToString(), out _))
+				{
+					Temporary += Input[I];
+					continue;
+				}
+
+				for (int I2 = I; long.TryParse(Input[I2].ToString(), out _); I2++)
+				{
+					Temporary += Input[I2];
+					I++;
+				}
+
+				switch (Input[I])
+				{
+					case '*':
+						Result = (long.Parse(Result) * long.Parse(Temporary)).ToString();
+						break;
+					case '/':
+						Result = (long.Parse(Result) / long.Parse(Temporary)).ToString();
+						break;
+					case '+':
+						Result = (long.Parse(Result) + long.Parse(Temporary)).ToString();
+						break;
+					case '-':
+						Result = (long.Parse(Result) - long.Parse(Temporary)).ToString();
+						break;
+					default:
+						continue;
+				}
+			}
+
+			return Result;
+		}
+	}
+}
