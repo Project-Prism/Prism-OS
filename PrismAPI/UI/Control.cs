35eea1082e0721c7aa8b870ce12bd436a5b561ce
Terminal.cs <evanlawrence@cyberfear.com>
2023-06-11 19:08:32 -0700

    Fix formatting 3

--- PrismAPI/UI/Control.cs
+++ PrismAPI/UI/Control.cs
@@ -6,41 +6,41 @@ namespace PrismAPI.UI;
 
 public abstract class Control : Canvas
 {
-    #region Constructors
+	#region Constructors
 
-    internal Control(ushort Width, ushort Height) : base(Width, Height)
-    {
-        OnClick = new((int _, int _, MouseState _) => { });
-        Layout = LayoutStyle.None;
-        Theme = ThemeStyle.None;
-        BackGround = Color.White;
-        ForeGround = Color.Black;
-        IsEnabled = true;
-        Radius = 0;
-    }
+	internal Control(ushort Width, ushort Height) : base(Width, Height)
+	{
+		OnClick = new((int _, int _, MouseState _) => { });
+		Layout = LayoutStyle.None;
+		Theme = ThemeStyle.None;
+		BackGround = Color.White;
+		ForeGround = Color.Black;
+		IsEnabled = true;
+		Radius = 0;
+	}
 
-    #endregion
+	#endregion
 
-    #region Methods
+	#region Methods
 
-    public abstract void Update(Canvas Canvas);
+	public abstract void Update(Canvas Canvas);
 
-    #endregion
+	#endregion
 
-    #region Fields
+	#region Fields
 
-    // The onclick event - Runs when the element is clicked on screen.
-    public Action<int, int, MouseState> OnClick;
+	// The onclick event - Runs when the element is clicked on screen.
+	public Action<int, int, MouseState> OnClick;
 
-    public CursorStatus Status;
+	public CursorStatus Status;
 	public LayoutStyle Layout;
-    public ThemeStyle Theme;
-    public Color BackGround;
-    public Color ForeGround;
+	public ThemeStyle Theme;
+	public Color BackGround;
+	public Color ForeGround;
 	public bool IsEnabled;
 	public ushort Radius;
-    public int X;
-    public int Y;
+	public int X;
+	public int Y;
 
-    #endregion
+	#endregion
 }
